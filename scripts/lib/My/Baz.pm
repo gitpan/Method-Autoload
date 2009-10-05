a403b0f3c54c87a0a51c7016bea7851335219a65
Michael R. Davis <mrdvt@cpan.org>
2009-10-05 03:22:11 +0000

    Import of MRDVT/Method-Autoload-0.01 from CPAN.
    gitpan-cpan-distribution: Method-Autoload
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MRDVT/Method-Autoload-0.01.tar.gz
    gitpan-cpan-author:       MRDVT
    gitpan-cpan-maturity:     released

---
+++ scripts/lib/My/Baz.pm
@@ -0,0 +1,7 @@
+package My::Baz;
+use strict;
+use warnings;
+sub foo {"My::Baz::foo"};
+sub bar {"My::Baz::bar"};
+sub baz {"My::Baz::baz"};
+1;
---
+++ scripts/lib/My/Fuz.pm
@@ -0,0 +1,5 @@
+package My::Fuz;
+use strict;
+use warnings;
+sub fuz {"My::Fuz::fuz"};
+1;
